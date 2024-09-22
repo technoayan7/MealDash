849e3e7bbe68a1d6fceb7d668e1f0c82673b81d9
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-09-22 06:31:22 +0530

    Update MyOrders.jsx

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -27,28 +27,32 @@ const MyOrders = () => {
     <div className="my-orders">
       <h2 className="myordersp">My Orders</h2>
       <div className="container">
-        {data.map((order, index) => {
-          return (
-            <div key={index} className="my-orders-order">
-              <img src={assets.parcel_icon} alt="" />
-              <p>
-                {order.items.map((item, index) => {
-                  if (index === order.items.length - 1) {
-                    return item.name + " x " + item.quantity;
-                  } else {
-                    return item.name + " x " + item.quantity + ",";
-                  }
-                })}
-              </p>
-              <p>${order.amount}.00</p>
-              <p>Items: {order.items.length}</p>
-              <p>
-                <span>&#x25cf;</span> <b>{order.status}</b>
-              </p>
-              <button onClick={fetchOrders}>Track Order</button>
-            </div>
-          );
-        })}
+        {data
+          .slice()
+          .reverse()
+          .map((order, index) => {
+            return (
+              <div key={index} className="my-orders-order">
+                <img src={assets.parcel_icon} alt="" />
+                <p>
+                  {order.items.map((item, index) => {
+                    if (index === order.items.length - 1) {
+                      return item.name + " x " + item.quantity;
+                    } else {
+                      return item.name + " x " + item.quantity + ",";
+                    }
+                  })}
+                </p>
+                {/* Ensure amount is shown with 2 decimal places */}
+                <p>${order.amount.toFixed(2)}</p>
+                <p>Items: {order.items.length}</p>
+                <p>
+                  <span>&#x25cf;</span> <b>{order.status}</b>
+                </p>
+                <button onClick={fetchOrders}>Track Order</button>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
