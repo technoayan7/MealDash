220371269817bba855592fcde034d98c4844bff0
Ayan Ahmad <87315325+technoayan7@users.noreply.github.com>
2024-10-02 01:04:35 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./Context/StoreContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId="1023660269721-do533ov4fe6m5nuieb8gkft77s2bdgog.apps.googleusercontent.com">
     <BrowserRouter>
       <StoreContextProvider>
         <App />
       </StoreContextProvider>
     </BrowserRouter>
+  </GoogleOAuthProvider>
 );
